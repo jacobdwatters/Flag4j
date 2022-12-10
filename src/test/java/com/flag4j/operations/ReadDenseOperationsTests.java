a52af79b9e3990f5864802eed3e0072080f6a4a9
jacob <jacobdwatters@gmail.com>
2022-12-10 12:08:45 -0700

    Complete refactor of library.
    All tensors, matrices, and vectors now store their data in a 1d array.
    
    All tensors, matrices, and vectors extend the TensorBase class.

--- src/test/java/com/flag4j/operations/ReadDenseOperationsTests.java
+++ src/test/java/com/flag4j/operations/ReadDenseOperationsTests.java
@@ -1,5 +1,8 @@
 package com.flag4j.operations;
 
+import com.flag4j.Shape;
+import com.flag4j.Tensor;
+import com.flag4j.core.TensorBase;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static com.flag4j.operations.RealDenseOperations.*;
@@ -9,6 +12,9 @@ class ReadDenseOperationsTests {
     double[] src1, src2;
     double[] expResult;
     double expResultD;
+    Tensor A, B;
+    Shape shapeA;
+    Shape shapeB;
 
 
     @Test
@@ -18,17 +24,32 @@ class ReadDenseOperationsTests {
         src2 = new double[]{9.1233, 22, 0.00009234, 11.234, -88.1, 13.4};
         expResult = new double[]{1+9.1233, 0.98332+22, 134.556+0.00009234,
                 -9.13+11.234, -100.234-88.1, 0.0000000004+13.4};
-        assertArrayEquals(expResult, add(src1, src2));
+        shapeA = new Shape(src1.length);
+        shapeB = new Shape(src2.length);
+        A = new Tensor(shapeA, src1);
+        B = new Tensor(shapeB, src2);
+
+        assertArrayEquals(expResult, add(A, B));
 
         // ---------- Sub-case 2 -----------------
         src1 = new double[]{1, 0.98332, 134.556, -9.13, -100.234, 0.0000000004};
         src2 = new double[]{9.1233, 22, 0.00009234, 11.234, -88.1, 13.4, 1, 1};
-        assertThrows(IllegalArgumentException.class, () -> add(src1, src2));
+        shapeA = new Shape(src1.length);
+        shapeB = new Shape(src2.length);
+        A = new Tensor(shapeA, src1);
+        B = new Tensor(shapeB, src2);
+
+        assertThrows(IllegalArgumentException.class, () -> add(A, B));
 
         // ---------- Sub-case 3 -----------------
         src1 = new double[]{1, 0.98332, 134.556, -9.13, -100.234, 0.0000000004};
         src2 = new double[]{9.1233, 22, 0.00009234, 11.234};
-        assertThrows(IllegalArgumentException.class, () -> add(src1, src2));
+        shapeA = new Shape(src1.length);
+        shapeB = new Shape(src2.length);
+        A = new Tensor(shapeA, src1);
+        B = new Tensor(shapeB, src2);
+
+        assertThrows(IllegalArgumentException.class, () -> add(A, B));
     }
 
 
