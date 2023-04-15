14ad2da95b2fdba6faac0e53006284a888058161
jacob <jacobdwatters@gmail.com>
2023-04-15 16:02:13 -0600

    Moved test to mirror src module.

---
+++ src/test/java/com/flag4j/CVectorStackJoinExtendTest.java
@@ -0,0 +1,404 @@
+package com.flag4j;
+
+import com.flag4j.*;
+import com.flag4j.complex_numbers.CNumber;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CVectorStackJoinExtendTest {
+
+    static CNumber[] aEntries;
+    static CVector a;
+
+    int[] sparseIndices;
+    int sparseSize;
+
+    @BeforeAll
+    static void setup() {
+        aEntries = new CNumber[]{new CNumber(1.455, 6126.347), new CNumber(-9.234, 5.0),
+                new CNumber(9.245, -56.2345), new CNumber(0, 14.5), new CNumber(-0.009257)};
+        a = new CVector(aEntries);
+    }
+
+
+    @Test
+    void joinRealDenseTest() {
+        double[] bEntries;
+        Vector b;
+        CNumber[] expEntries;
+        CVector exp;
+
+        // ---------------------- Sub-case 1 ----------------------
+        bEntries = new double[]{89.24, 5.66};
+        b = new Vector(bEntries);
+        expEntries = new CNumber[]{new CNumber(1.455, 6126.347), new CNumber(-9.234, 5.0),
+                new CNumber(9.245, -56.2345), new CNumber(0, 14.5), new CNumber(-0.009257),
+                new CNumber(89.24), new CNumber(5.66)
+        };
+        exp = new CVector(expEntries);
+
+        assertEquals(exp, a.join(b));
+    }
+
+
+    @Test
+    void joinRealSparseTest() {
+        double[] bEntries;
+        SparseVector b;
+        CNumber[] expEntries;
+        CVector exp;
+
+        // ---------------------- Sub-case 1 ----------------------
+        bEntries = new double[]{89.24, 5.66};
+        sparseSize = 4;
+        sparseIndices = new int[]{0, 2};
+        b = new SparseVector(sparseSize, bEntries, sparseIndices);
+        expEntries = new CNumber[]{new CNumber(1.455, 6126.347), new CNumber(-9.234, 5.0),
+                new CNumber(9.245, -56.2345), new CNumber(0, 14.5), new CNumber(-0.009257),
+                new CNumber(89.24), new CNumber(), new CNumber( 5.66), new CNumber()
+        };
+        exp = new CVector(expEntries);
+
+        assertEquals(exp, a.join(b));
+    }
+
+
+    @Test
+    void joinComplexDenseTest() {
+        CNumber[] bEntries, expEntries;
+        CVector b, exp;
+
+        // ---------------------- Sub-case 1 ----------------------
+        bEntries = new CNumber[]{new CNumber(2.4656, 9.24), new CNumber(-0.9924, -0.01)};
+        b = new CVector(bEntries);
+        expEntries = new CNumber[]{new CNumber(1.455, 6126.347), new CNumber(-9.234, 5.0),
+                new CNumber(9.245, -56.2345), new CNumber(0, 14.5), new CNumber(-0.009257),
+                new CNumber(2.4656, 9.24), new CNumber(-0.9924, -0.01)
+        };
+        exp = new CVector(expEntries);
+
+        assertEquals(exp, a.join(b));
+    }
+
+
+    @Test
+    void joinComplexSparseTest() {
+        CNumber[] bEntries, expEntries;
+        CVector exp;
+        SparseCVector b;
+
+        // ---------------------- Sub-case 1 ----------------------
+        bEntries = new CNumber[]{new CNumber(2.4656, 9.24)};
+        sparseSize = 3;
+        sparseIndices = new int[]{2};
+        b = new SparseCVector(sparseSize, bEntries, sparseIndices);
+        expEntries = new CNumber[]{new CNumber(1.455, 6126.347), new CNumber(-9.234, 5.0),
+                new CNumber(9.245, -56.2345), new CNumber(0, 14.5), new CNumber(-0.009257),
+                new CNumber(), new CNumber(), new CNumber(2.4656, 9.24)
+        };
+        exp = new CVector(expEntries);
+
+        assertEquals(exp, a.join(b));
+    }
+
+
+    @Test
+    void stackRealDenseTest() {
+        double[] bEntries;
+        Vector b;
+
+        CNumber[][] expEntries;
+        CMatrix exp;
+
+        // ---------------------- Sub-case 1 ----------------------
+        bEntries = new double[]{5.46, -973.4, 0.0034, 15.6, 0};
+        b = new Vector(bEntries);
+        expEntries = new CNumber[][]{
+                {new CNumber(1.455, 6126.347), new CNumber(-9.234, 5.0),
+                        new CNumber(9.245, -56.2345), new CNumber(0, 14.5), new CNumber(-0.009257)},
+                {new CNumber(5.46), new CNumber(-973.4), new CNumber(0.0034), new CNumber(15.6), new CNumber()}
+        };
+        exp = new CMatrix(expEntries);
+
+        assertEquals(exp, a.stack(b));
+
+        // ---------------------- Sub-case 2 ----------------------
+        bEntries = new double[]{5.46, -973.4, 0.0034, 15.6, 0, 8255.6668, 0.0009245};
+        b = new Vector(bEntries);
+
+        Vector finalB = b;
+        assertThrows(IllegalArgumentException.class, ()->a.stack(finalB));
+
+        // ---------------------- Sub-case 3 ----------------------
+        bEntries = new double[]{5.46, -973.4, 0.0034, 15.6, 0};
+        b = new Vector(bEntries);
+        expEntries = new CNumber[][]{
+                {new CNumber(1.455, 6126.347), new CNumber(-9.234, 5.0),
+                        new CNumber(9.245, -56.2345), new CNumber(0, 14.5), new CNumber(-0.009257)},
+                {new CNumber(5.46), new CNumber(-973.4), new CNumber(0.0034), new CNumber(15.6), new CNumber()}
+        };
+        exp = new CMatrix(expEntries);
+
+        assertEquals(exp, a.stack(b, 0));
+
+        // ---------------------- Sub-case 4 ----------------------
+        bEntries = new double[]{5.46, -973.4, 0.0034, 15.6, 0, 8255.6668, 0.0009245};
+        b = new Vector(bEntries);
+
+        Vector finalB2 = b;
+        assertThrows(IllegalArgumentException.class, ()->a.stack(finalB2, 0));
+
+        // ---------------------- Sub-case 5 ----------------------
+        bEntries = new double[]{5.46, -973.4, 0.0034, 15.6, 0};
+        b = new Vector(bEntries);
+        expEntries = new CNumber[][]{
+                {new CNumber(1.455, 6126.347), new CNumber(5.46)},
+                        {new CNumber(-9.234, 5.0), new CNumber(-973.4)},
+                        {new CNumber(9.245, -56.2345), new CNumber(0.0034)},
+                        {new CNumber(0, 14.5), new CNumber(15.6)},
+                        {new CNumber(-0.009257), new CNumber()}};
+        exp = new CMatrix(expEntries);
+
+        assertEquals(exp, a.stack(b, 1));
+
+        // ---------------------- Sub-case 6 ----------------------
+        bEntries = new double[]{5.46, -973.4, 0.0034, 15.6, 0, 8255.6668, 0.0009245};
+        b = new Vector(bEntries);
+
+        Vector finalB3 = b;
+        assertThrows(IllegalArgumentException.class, ()->a.stack(finalB3, 1));
+    }
+
+
+    @Test
+    void stackRealSparseTest() {
+        double[] bEntries;
+        SparseVector b;
+
+        CNumber[][] expEntries;
+        CMatrix exp;
+
+        // ---------------------- Sub-case 1 ----------------------
+        bEntries = new double[]{-78.336, 0.00234};
+        sparseSize = 5;
+        sparseIndices = new int[]{1, 4};
+        b = new SparseVector(sparseSize, bEntries, sparseIndices);
+        expEntries = new CNumber[][]{
+                {new CNumber(1.455, 6126.347), new CNumber(-9.234, 5.0),
+                        new CNumber(9.245, -56.2345), new CNumber(0, 14.5), new CNumber(-0.009257)},
+                {new CNumber(), new CNumber(-78.336), new CNumber(), new CNumber(), new CNumber(0.00234)}
+        };
+        exp = new CMatrix(expEntries);
+
+        assertEquals(exp, a.stack(b));
+
+        // ---------------------- Sub-case 2 ----------------------
+        bEntries = new double[]{-78.336, 0.00234};
+        sparseSize = 355615;
+        sparseIndices = new int[]{1, 4};
+        b = new SparseVector(sparseSize, bEntries, sparseIndices);
+
+        SparseVector finalB = b;
+        assertThrows(IllegalArgumentException.class, ()->a.stack(finalB));
+
+        // ---------------------- Sub-case 3 ----------------------
+        bEntries = new double[]{-78.336, 0.00234};
+        sparseSize = 5;
+        sparseIndices = new int[]{1, 4};
+        b = new SparseVector(sparseSize, bEntries, sparseIndices);
+        expEntries = new CNumber[][]{
+                {new CNumber(1.455, 6126.347), new CNumber(-9.234, 5.0),
+                        new CNumber(9.245, -56.2345), new CNumber(0, 14.5), new CNumber(-0.009257)},
+                {new CNumber(), new CNumber(-78.336), new CNumber(), new CNumber(), new CNumber(0.00234)}
+        };
+        exp = new CMatrix(expEntries);
+
+        assertEquals(exp, a.stack(b, 0));
+
+        // ---------------------- Sub-case 4 ----------------------
+        bEntries = new double[]{-78.336, 0.00234};
+        sparseSize = 355615;
+        sparseIndices = new int[]{1, 4};
+        b = new SparseVector(sparseSize, bEntries, sparseIndices);
+
+        SparseVector finalB2 = b;
+        assertThrows(IllegalArgumentException.class, ()->a.stack(finalB2, 0));
+
+        // ---------------------- Sub-case 5 ----------------------
+        bEntries = new double[]{-78.336, 0.00234};
+        sparseSize = 5;
+        sparseIndices = new int[]{1, 4};
+        b = new SparseVector(sparseSize, bEntries, sparseIndices);
+        expEntries = new CNumber[][]{
+                {new CNumber(1.455, 6126.347), new CNumber()},
+                {new CNumber(-9.234, 5.0), new CNumber(-78.336)},
+                {new CNumber(9.245, -56.2345), new CNumber()},
+                {new CNumber(0, 14.5), new CNumber()},
+                {new CNumber(-0.009257), new CNumber(0.00234)}};
+        exp = new CMatrix(expEntries);
+
+        assertEquals(exp, a.stack(b, 1));
+
+        // ---------------------- Sub-case 6 ----------------------
+        bEntries = new double[]{-78.336, 0.00234};
+        sparseSize = 355615;
+        sparseIndices = new int[]{1, 4};
+        b = new SparseVector(sparseSize, bEntries, sparseIndices);
+
+        SparseVector finalB3 = b;
+        assertThrows(IllegalArgumentException.class, ()->a.stack(finalB3, 1));
+    }
+
+
+    @Test
+    void stackComplexDenseTest() {
+        CNumber[] bEntries;
+        CVector b;
+
+        CNumber[][] expEntries;
+        CMatrix exp;
+
+        // ---------------------- Sub-case 1 ----------------------
+        bEntries = new CNumber[]{new CNumber(2.4656, 9.24), new CNumber(-0.9924, -0.01),
+        new CNumber(0, 1405.24), new CNumber(9.356), new CNumber(0.245, -8824.5)};
+        b = new CVector(bEntries);
+        expEntries = new CNumber[][]{
+                {new CNumber(1.455, 6126.347), new CNumber(-9.234, 5.0),
+                        new CNumber(9.245, -56.2345), new CNumber(0, 14.5), new CNumber(-0.009257)},
+                {new CNumber(2.4656, 9.24), new CNumber(-0.9924, -0.01),
+                        new CNumber(0, 1405.24), new CNumber(9.356), new CNumber(0.245, -8824.5)}
+        };
+        exp = new CMatrix(expEntries);
+
+        assertEquals(exp, a.stack(b));
+        assertEquals(exp, a.stack(b, 0));
+
+        // ---------------------- Sub-case 2 ----------------------
+        bEntries = new CNumber[]{new CNumber(2.4656, 9.24), new CNumber(-0.9924, -0.01),
+                new CNumber(0, 1405.24)};
+        b = new CVector(bEntries);
+
+        CVector finalB = b;
+        assertThrows(IllegalArgumentException.class, ()->a.stack(finalB));
+        assertThrows(IllegalArgumentException.class, ()->a.stack(finalB, 0));
+
+        // ---------------------- Sub-case 3 ----------------------
+        bEntries = new CNumber[]{new CNumber(2.4656, 9.24), new CNumber(-0.9924, -0.01),
+                new CNumber(0, 1405.24), new CNumber(9.356), new CNumber(0.245, -8824.5)};
+        b = new CVector(bEntries);
+        expEntries = new CNumber[][]{
+                {new CNumber(1.455, 6126.347), new CNumber(2.4656, 9.24)},
+                {new CNumber(-9.234, 5.0), new CNumber(-0.9924, -0.01)},
+                {new CNumber(9.245, -56.2345), new CNumber(0, 1405.24)},
+                {new CNumber(0, 14.5), new CNumber(9.356)},
+                {new CNumber(-0.009257),new CNumber(0.245, -8824.5) }};
+        exp = new CMatrix(expEntries);
+
+        assertEquals(exp, a.stack(b, 1));
+
+        // ---------------------- Sub-case 4 ----------------------
+        bEntries = new CNumber[]{new CNumber(2.4656, 9.24), new CNumber(-0.9924, -0.01),
+                new CNumber(0, 1405.24)};
+        b = new CVector(bEntries);
+
+        CVector finalB2 = b;
+        assertThrows(IllegalArgumentException.class, ()->a.stack(finalB2, 1));
+    }
+
+
+    @Test
+    void stackComplexSparseTest() {
+        CNumber[] bEntries;
+        SparseCVector b;
+
+        CNumber[][] expEntries;
+        CMatrix exp;
+
+        // ---------------------- Sub-case 1 ----------------------
+        bEntries = new CNumber[]{new CNumber(2.4656, 9.24)};
+        sparseIndices = new int[]{2};
+        sparseSize = 5;
+        b = new SparseCVector(sparseSize, bEntries, sparseIndices);
+        expEntries = new CNumber[][]{
+                {new CNumber(1.455, 6126.347), new CNumber(-9.234, 5.0),
+                        new CNumber(9.245, -56.2345), new CNumber(0, 14.5), new CNumber(-0.009257)},
+                {new CNumber(), new CNumber(), new CNumber(2.4656, 9.24), new CNumber(), new CNumber()}
+        };
+        exp = new CMatrix(expEntries);
+
+        assertEquals(exp, a.stack(b));
+        assertEquals(exp, a.stack(b, 0));
+
+        // ---------------------- Sub-case 2 ----------------------
+        bEntries = new CNumber[]{new CNumber(2.4656, 9.24)};
+        sparseIndices = new int[]{2};
+        sparseSize = 42;
+        b = new SparseCVector(sparseSize, bEntries, sparseIndices);
+
+        SparseCVector finalB = b;
+        assertThrows(IllegalArgumentException.class, ()->a.stack(finalB));
+        assertThrows(IllegalArgumentException.class, ()->a.stack(finalB, 0));
+
+        // ---------------------- Sub-case 3 ----------------------
+        bEntries = new CNumber[]{new CNumber(2.4656, 9.24)};
+        sparseIndices = new int[]{2};
+        sparseSize = 5;
+        b = new SparseCVector(sparseSize, bEntries, sparseIndices);
+        expEntries = new CNumber[][]{
+                {new CNumber(1.455, 6126.347), new CNumber()},
+                {new CNumber(-9.234, 5.0), new CNumber()},
+                {new CNumber(9.245, -56.2345), new CNumber(2.4656, 9.24)},
+                {new CNumber(0, 14.5), new CNumber()},
+                {new CNumber(-0.009257), new CNumber()}};
+        exp = new CMatrix(expEntries);
+
+        assertEquals(exp, a.stack(b, 1));
+
+        // ---------------------- Sub-case 4 ----------------------
+        bEntries = new CNumber[]{new CNumber(2.4656, 9.24)};
+        sparseIndices = new int[]{2};
+        sparseSize = 42;
+        b = new SparseCVector(sparseSize, bEntries, sparseIndices);
+
+        SparseCVector finalB2 = b;
+        assertThrows(IllegalArgumentException.class, ()->a.stack(finalB2, 1));
+    }
+
+
+    @Test
+    void extendTest() {
+        CNumber[][] expEntries;
+        CMatrix exp;
+
+        // ---------------------- Sub-case 1 ----------------------
+        expEntries = new CNumber[][]{{new CNumber(1.455, 6126.347), new CNumber(-9.234, 5.0),
+                new CNumber(9.245, -56.2345), new CNumber(0, 14.5), new CNumber(-0.009257)}};
+        exp = new CMatrix(expEntries);
+
+        assertEquals(exp, a.extend(1, 0));
+
+        // ---------------------- Sub-case 2 ----------------------
+        expEntries = new CNumber[][]{{new CNumber(1.455, 6126.347), new CNumber(-9.234, 5.0),
+                new CNumber(9.245, -56.2345), new CNumber(0, 14.5), new CNumber(-0.009257)},
+                {new CNumber(1.455, 6126.347), new CNumber(-9.234, 5.0),
+                        new CNumber(9.245, -56.2345), new CNumber(0, 14.5), new CNumber(-0.009257)},
+                {new CNumber(1.455, 6126.347), new CNumber(-9.234, 5.0),
+                        new CNumber(9.245, -56.2345), new CNumber(0, 14.5), new CNumber(-0.009257)}};
+        exp = new CMatrix(expEntries);
+
+        assertEquals(exp, a.extend(3, 0));
+
+        // ---------------------- Sub-case 3 ----------------------
+        expEntries = new CNumber[][]{{new CNumber(1.455, 6126.347), new CNumber(1.455, 6126.347), new CNumber(1.455, 6126.347)},
+                {new CNumber(-9.234, 5.0), new CNumber(-9.234, 5.0), new CNumber(-9.234, 5.0)},
+                {new CNumber(9.245, -56.2345), new CNumber(9.245, -56.2345), new CNumber(9.245, -56.2345)},
+                {new CNumber(0, 14.5), new CNumber(0, 14.5), new CNumber(0, 14.5)},
+                {new CNumber(-0.009257), new CNumber(-0.009257), new CNumber(-0.009257)}};
+        exp = new CMatrix(expEntries);
+
+        assertEquals(exp, a.extend(3, 1));
+    }
+}
---
+++ src/test/java/com/flag4j/CVectorZeroOneTests.java
@@ -0,0 +1,101 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 Jacob Watters
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.flag4j;
+
+import com.flag4j.CVector;
+import com.flag4j.complex_numbers.CNumber;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CVectorZeroOneTests {
+
+    CNumber[] aEntries;
+    CVector a;
+
+    @Test
+    void zerosTest() {
+        // ------------------ Sub-case 1 ------------------
+        a = new CVector(34);
+        assertTrue(a.isZeros());
+
+        // ------------------ Sub-case 2 ------------------
+        a = new CVector(0);
+        assertTrue(a.isZeros());
+
+        // ------------------ Sub-case 3 ------------------
+        aEntries = new CNumber[]{new CNumber(), new CNumber(), new CNumber()};
+        a = new CVector(aEntries);
+        assertTrue(a.isZeros());
+
+        // ------------------ Sub-case 4 ------------------
+        aEntries = new CNumber[]{new CNumber(), new CNumber(1), new CNumber()};
+        a = new CVector(aEntries);
+        assertFalse(a.isZeros());
+
+        // ------------------ Sub-case 5 ------------------
+        aEntries = new CNumber[]{new CNumber(), new CNumber(9.4, -6.233), new CNumber()};
+        a = new CVector(aEntries);
+        assertFalse(a.isZeros());
+
+        // ------------------ Sub-case 6 ------------------
+        aEntries = new CNumber[]{new CNumber(), new CNumber(), new CNumber(0, -8.234)};
+        a = new CVector(aEntries);
+        assertFalse(a.isZeros());
+    }
+
+
+    @Test
+    void onesTest() {
+        // ------------------ Sub-case 1 ------------------
+        a = new CVector(34, 1);
+        assertTrue(a.isOnes());
+
+        // ------------------ Sub-case 2 ------------------
+        a = new CVector(0, 1);
+        assertTrue(a.isOnes());
+
+        // ------------------ Sub-case 3 ------------------
+        aEntries = new CNumber[]{new CNumber(1), new CNumber(1), new CNumber(1)};
+        a = new CVector(aEntries);
+        assertTrue(a.isOnes());
+
+        // ------------------ Sub-case 4 ------------------
+        aEntries = new CNumber[]{new CNumber(1), new CNumber(1.2), new CNumber(1)};
+        a = new CVector(aEntries);
+        assertFalse(a.isOnes());
+
+        // ------------------ Sub-case 5 ------------------
+        aEntries = new CNumber[]{new CNumber(1), new CNumber(5.3, 91.3), new CNumber(1)};
+        a = new CVector(aEntries);
+        assertFalse(a.isOnes());
+
+        // ------------------ Sub-case 6 ------------------
+        aEntries = new CNumber[]{new CNumber(1), new CNumber(1), new CNumber(1, -1)};
+        a = new CVector(aEntries);
+        assertFalse(a.isOnes());
+    }
+}
