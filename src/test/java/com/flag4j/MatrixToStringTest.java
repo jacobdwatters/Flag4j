b4b7e9894f8468fc3c0a9b27b981e3956ea7a4cb
jacob <jacobdwatters@gmail.com>
2023-06-09 17:12:42 -0600

    Update tests

---
+++ src/test/java/com/flag4j/MatrixToStringTest.java
@@ -0,0 +1,144 @@
+package com.flag4j;
+
+import com.flag4j.Matrix;
+import com.flag4j.io.PrintOptions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MatrixToStringTest {
+
+    double[][] aEntries;
+    Matrix A;
+    String exp;
+
+
+    @Test
+    void toStringTest() {
+        // ------------------------ Sub-case 1 ------------------------
+        aEntries = new double[][]{
+                {1.32239898234, 2.46560324, 1.45, -0.11234},
+                {0.000324, 1.456, -123.4, 2341.56}};
+        A = new Matrix(aEntries);
+        PrintOptions.setPrecision(50);
+        PrintOptions.setPadding(2);
+        PrintOptions.setMaxRows(50);
+        PrintOptions.setMaxColumns(50);
+        PrintOptions.setCentering(true);
+        exp = "[[ 1.32239898234  2.46560324   1.45   -0.11234 ]\n" +
+                " [    3.24E-4       1.456     -123.4  2341.56  ]]";
+
+        assertEquals(exp, A.toString());
+
+        // ------------------------ Sub-case 2 ------------------------
+        aEntries = new double[][]{
+                {1.32239898234, 2.46560324, 1.45, -0.11234},
+                {0.000324, 1.456, -123.4, 2341.56}};
+        A = new Matrix(aEntries);
+        PrintOptions.setPrecision(3);
+        PrintOptions.setPadding(2);
+        PrintOptions.setMaxRows(50);
+        PrintOptions.setMaxColumns(50);
+        PrintOptions.setCentering(true);
+        exp = "[[ 1.322  2.466   1.45   -0.112  ]\n" +
+                " [   0    1.456  -123.4  2341.56 ]]";
+
+        assertEquals(exp, A.toString());
+
+        // ------------------------ Sub-case 3 ------------------------
+        aEntries = new double[][]{
+                {1.32239898234, 2.46560324, 1.45, -0.11234},
+                {0.000324, 1.456, -123.4, 2341.56}};
+        A = new Matrix(aEntries);
+        PrintOptions.setPrecision(0);
+        PrintOptions.setPadding(4);
+        PrintOptions.setMaxRows(50);
+        PrintOptions.setMaxColumns(50);
+        PrintOptions.setCentering(true);
+        exp = "[[  1    2     1       0    ]\n" +
+                " [  0    1    -123    2342  ]]";
+
+        assertEquals(exp, A.toString());
+
+        // ------------------------ Sub-case 4 ------------------------
+        aEntries = new double[][]{
+                {1.32239898234, 2.46560324, 1.45, -0.11234},
+                {0.000324, 1.456, -123.4, 2341.56}};
+        A = new Matrix(aEntries);
+        PrintOptions.setPrecision(2);
+        PrintOptions.setPadding(2);
+        PrintOptions.setMaxRows(2);
+        PrintOptions.setMaxColumns(4);
+        PrintOptions.setCentering(true);
+        exp = "[[ 1.32  2.47   1.45    -0.11  ]\n" +
+                " [  0    1.46  -123.4  2341.56 ]]";
+
+        assertEquals(exp, A.toString());
+
+        // ------------------------ Sub-case 5 ------------------------
+        aEntries = new double[][]{
+                {1.32239898234, 2.46560324, 1.45, -0.11234},
+                {0.000324, 1.456, -123.4, 2341.56}};
+        A = new Matrix(aEntries);
+        PrintOptions.setPrecision(2);
+        PrintOptions.setPadding(2);
+        PrintOptions.setMaxRows(1);
+        PrintOptions.setMaxColumns(4);
+        PrintOptions.setCentering(true);
+        exp = "Full Shape: 2x4\n" +
+                "[ [           ...            ]\r\n" +
+                " [ 0  1.46  -123.4  2341.56 ]]";
+
+        assertEquals(exp, A.toString());
+
+        // ------------------------ Sub-case 6 ------------------------
+        aEntries = new double[][]{
+                {1.32239898234, 2.46560324, 1.45, -0.11234},
+                {0.000324, 1.456, -123.4, 2341.56}};
+        A = new Matrix(aEntries);
+        PrintOptions.setPrecision(2);
+        PrintOptions.setPadding(2);
+        PrintOptions.setMaxRows(2);
+        PrintOptions.setMaxColumns(3);
+        PrintOptions.setCentering(true);
+        exp = "Full Shape: 2x4\n" +
+                "[[ 1.32  2.47  ...   -0.11  ]\n" +
+                " [  0    1.46  ...  2341.56 ]]";
+
+        assertEquals(exp, A.toString());
+
+        // ------------------------ Sub-case 6 ------------------------
+        aEntries = new double[][]{
+                {1.32239898234, 2.46560324, 1.45, -0.11234},
+                {0.000324, 1.456, -123.4, 2341.56}};
+        A = new Matrix(aEntries);
+        PrintOptions.setPrecision(2);
+        PrintOptions.setPadding(2);
+        PrintOptions.setMaxRows(2);
+        PrintOptions.setMaxColumns(2);
+        PrintOptions.setCentering(true);
+        exp = "Full Shape: 2x4\n" +
+                "[[ 1.32  ...   -0.11  ]\n" +
+                " [  0    ...  2341.56 ]]";
+
+        assertEquals(exp, A.toString());
+
+        // ------------------------ Sub-case 6 ------------------------
+        aEntries = new double[][]{
+                {1.32239898234, 2.46560324, 1.45, -0.11234},
+                {0.000324, 1.456, -123.4, 2341.56}};
+        A = new Matrix(aEntries);
+        PrintOptions.setPrecision(3);
+        PrintOptions.setPadding(3);
+        PrintOptions.setMaxRows(2);
+        PrintOptions.setMaxColumns(4);
+        PrintOptions.setCentering(false);
+        exp = "[[1.322   2.466   1.45     -0.112    ]\n" +
+                " [0       1.456   -123.4   2341.56   ]]";
+
+        assertEquals(exp, A.toString());
+
+        // ------------------------ RESET PRINT OPTIONS ------------------------
+        PrintOptions.resetAll();
+    }
+}
