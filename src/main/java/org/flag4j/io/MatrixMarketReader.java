16d2bc13705a5c4016a3bb1a814188a7a61a407a
jacob <jacobdwatters@gmail.com>
2025-02-22 13:41:24 -0700

    Improved Matrix Market support.
    - Added support for dense/coo symmetric/Hermitian matrices (reading and writing).
    - Added support for coo pattern matrices (reading and writing.

--- src/main/java/org/flag4j/io/MatrixMarketReader.java
+++ src/main/java/org/flag4j/io/MatrixMarketReader.java
@@ -54,14 +54,22 @@ import java.util.function.Function;
  * <ul>
  *   <li>Object: matrix</li>
  *   <li>Format: coordinate, array</li>
- *   <li>Field: real, integer, complex</li>
- *   <li>MMSymmetry: general</li>
+ *   <li>Field: real, integer, complex, pattern</li>
+ *   <li>MMSymmetry: general, symmetric, Hermitian</li>
  * </ul>
- * <p>Headers must be of the form:
- *
- * <pre>
- * {@code %%MatrixMarket matrix <coordinate | array> <real | integer | complex> general}
- * </pre>
+ * <p>Headers must be of one of the following forms:
+ * <ul>
+ *     <li><pre>
+ *         {@code %%MatrixMarket matrix [coordinate | array] [real | integer | complex] [general | symmetric]}
+ *     </pre></li>
+ *     <li><pre>
+ *         {@code %%MatrixMarket matrix [coordinate | array> [complex> [general | symmetric | hermitian]}
+ *     </pre></li>
+ *     <li><pre>
+ *         {@code %%MatrixMarket matrix [coordinate] pattern [general | symmetric]}
+ *     </pre></li>
+ * </ul>
+ * All pattern matrices will be returned as a {@link CooMatrix real COO matrix} with all non-zero values set to {@code 1.0}.
  *
  * @see MatrixMarketHeader
  * @see org.flag4j.io.MatrixMarketWriter
@@ -194,11 +202,17 @@ public class MatrixMarketReader {
                 // TODO: Once primitive int matrices are implemented, have special case for integers.
                 if(symmetry == MatrixMarketHeader.MMSymmetry.GENERAL)
                     loadDenseRealMatrix();
+                else if(symmetry == MatrixMarketHeader.MMSymmetry.SYMMETRIC)
+                    loadSymmDenseRealMatrix();
                 else
                     throw new Flag4jParsingException(String.format(UNSUPPORTED_SYMMETRY_ERR, symmetry));
             } else if(field == MatrixMarketHeader.MMField.COMPLEX) {
                 if(symmetry == MatrixMarketHeader.MMSymmetry.GENERAL)
                     loadDenseComplexMatrix();
+                else if(symmetry == MatrixMarketHeader.MMSymmetry.SYMMETRIC)
+                    loadHermDenseComplexMatrix(false);
+                else if(symmetry == MatrixMarketHeader.MMSymmetry.HERMITIAN)
+                    loadHermDenseComplexMatrix(true);
                 else
                     throw new Flag4jParsingException(String.format(UNSUPPORTED_SYMMETRY_ERR, symmetry));
             } else {
@@ -208,12 +222,25 @@ public class MatrixMarketReader {
             if(field == MatrixMarketHeader.MMField.REAL || field == MatrixMarketHeader.MMField.INTEGER) {
                 // TODO: Once primitive int matrices are implemented, have special case for integers.
                 if(symmetry == MatrixMarketHeader.MMSymmetry.GENERAL)
-                    loadCooRealMatrix();
+                    loadCooRealMatrix(false);
+                else if(symmetry == MatrixMarketHeader.MMSymmetry.SYMMETRIC)
+                    loadSymmCooRealMatrix(false);
                 else
                     throw new Flag4jParsingException(String.format(UNSUPPORTED_SYMMETRY_ERR, symmetry));
             } else if(field == MatrixMarketHeader.MMField.COMPLEX) {
                 if(symmetry == MatrixMarketHeader.MMSymmetry.GENERAL)
-                    loadCooComplexMatrix();
+                    loadCooComplexMatrix(false);
+                else if(symmetry == MatrixMarketHeader.MMSymmetry.SYMMETRIC)
+                    loadHermCooComplexMatrix(false);
+                else if(symmetry == MatrixMarketHeader.MMSymmetry.HERMITIAN)
+                    loadHermCooComplexMatrix(true);
+                else
+                    throw new Flag4jParsingException(String.format(UNSUPPORTED_SYMMETRY_ERR, symmetry));
+            } else if(field == MatrixMarketHeader.MMField.PATTERN) {
+                if(symmetry == MatrixMarketHeader.MMSymmetry.GENERAL)
+                    loadCooRealMatrix(true);
+                else if(symmetry == MatrixMarketHeader.MMSymmetry.SYMMETRIC)
+                    loadSymmCooRealMatrix(true);
                 else
                     throw new Flag4jParsingException(String.format(UNSUPPORTED_SYMMETRY_ERR, symmetry));
             } else {
@@ -236,6 +263,82 @@ public class MatrixMarketReader {
 
         // Transpose to account for column major ordering in Matrix Market Format.
         mat = new Matrix(shape.swapAxes(0, 1), data).T();
+    }
+
+
+    /**
+     * Loads a real dense matrix from a Matrix Market Format file.
+     * @throws IOException If an I/O error occurs.
+     */
+    private void loadSymmDenseRealMatrix() throws IOException {
+        Pair<Shape, List<Double>> matData = loadDenseSymmMatrix(Double::parseDouble);
+        Shape shape = matData.first();
+        int size = shape.get(0);
+
+        // Get upper triangular data.
+        double[] lowerData = ArrayConversions.fromDoubleList(matData.second());
+        double[] data = new double[size*size];
+
+        int upperIndex = 0;
+        int rowOffset = 0;
+        // Copy data to both triangular sections of the matrix.
+        for(int j=0; j<size; j++) {
+            for(int i=j; i<size; i++) {
+                double value = lowerData[upperIndex++];
+                data[i*size + j] = value;
+                data[rowOffset + i] = value;
+            }
+
+            rowOffset += size;
+        }
+
+        // No need to transpose since matrix is symmetric.
+        mat = new Matrix(shape.swapAxes(0, 1), data);
+    }
+
+
+    /**
+     * Loads a real dense matrix from a Matrix Market Format file.
+     * @param isHerm Flag indicating if the matrix is Hermitian ({@code true}) or symmetric ({@code false}).
+     * @throws IOException If an I/O error occurs.
+     */
+    private void loadHermDenseComplexMatrix(boolean isHerm) throws IOException {
+        Function<String, Complex128> parseFunction = (String line) -> {
+            StringTokenizer tokenizer = new StringTokenizer(currLine);
+            double re = Double.parseDouble(tokenizer.nextToken());
+            double im = Double.parseDouble(tokenizer.nextToken());
+
+            if(tokenizer.hasMoreTokens())
+                throw new Flag4jParsingException("Expecting two values for complex data.");
+
+            return new Complex128(re, im);
+        };
+
+        Pair<Shape, List<Complex128>> matData = loadDenseSymmMatrix(parseFunction);
+        Shape shape = matData.first();
+        int size = shape.get(0);
+
+        // Get upper triangular data.
+        Complex128[] lowerData = matData.second().toArray(new Complex128[0]);
+        Complex128[] data = new Complex128[size*size];
+
+        int upperIndex = 0;
+        int rowOffset = 0;
+        // Copy data to both triangular sections of the matrix.
+        for(int j=0; j<size; j++) {
+            data[j*size + j] = lowerData[upperIndex++];
+
+            for(int i=j+1; i<size; i++) {
+                Complex128 value = lowerData[upperIndex++];
+                data[i*size + j] = value;
+                data[rowOffset + i] = isHerm ? value.conj() : value;
+            }
+
+            rowOffset += size;
+        }
+
+        // No need to transpose since matrix is symmetric.
+        mat = new CMatrix(shape.swapAxes(0, 1), data);
     }
 
 
@@ -266,24 +369,94 @@ public class MatrixMarketReader {
 
     /**
      * Loads a real COO matrix from a Matrix Market Format file.
+     * @param isPattern Flag indicating if this matrix is a pattern matrix ({@code true}) or a numerical matrix ({@code false}).
      * @throws IOException If an I/O error occurs.
      */
-    private void loadCooRealMatrix() throws IOException {
+    private void loadCooRealMatrix(boolean isPattern) throws IOException {
         Function<StringTokenizer, Double> parseFunction =
                 (StringTokenizer tokenizer) -> Double.parseDouble(tokenizer.nextToken());
-        SparseMatrixData<Double> matData = loadCooMatrix(parseFunction);
+        SparseMatrixData<Double> matData = loadCooMatrix(parseFunction, true, isPattern, 1d);
         mat = new CooMatrix(matData.shape(), matData.data(), matData.rowData(), matData.colData());
     }
 
 
     /**
-     * Loads a complex COO matrix from a Matrix Market Format file.
+     * Loads a real COO matrix from a Matrix Market Format file.
+     * @param isPattern Flag indicating if this matrix is a pattern matrix ({@code true}) or a numerical matrix ({@code false}).
      * @throws IOException If an I/O error occurs.
      */
-    private void loadCooComplexMatrix() throws IOException {
+    private void loadSymmCooRealMatrix(boolean isPattern) throws IOException {
+        Function<StringTokenizer, Double> parseFunction =
+                (StringTokenizer tokenizer) -> Double.parseDouble(tokenizer.nextToken());
+        SparseMatrixData<Double> matData = loadCooMatrix(parseFunction, false, isPattern, 1d);
+
+        if(!matData.shape().isSquare())
+            throw new Flag4jParsingException("Symmetric matrices must be square but got shape " + matData.shape());
+
+        List<Double> data = matData.data();
+        List<Integer> rowIndices = matData.rowData();
+        List<Integer> colIndices = matData.colData();
+        int size = data.size();
+
+        // Copy lower triangular values to upper triangular portion for symmetric matrix.
+        for(int i=0; i<size; i++) {
+            int row = rowIndices.get(i);
+            int col = colIndices.get(i);
+
+            if(row != col) {
+                data.add(data.get(i));
+                rowIndices.add(col);
+                colIndices.add(row);
+            }
+        }
+
+        mat = new CooMatrix(matData.shape(), data, rowIndices, colIndices).sortIndices();
+    }
+
+
+    /**
+     * Loads a real COO matrix from a Matrix Market Format file.
+     * @param isHerm Flag indicating if the matrix is Hermitian ({@code true}) or symmetric ({@code false}).
+     * @throws IOException If an I/O error occurs.
+     */
+    private void loadHermCooComplexMatrix(boolean isHerm) throws IOException {
+        Function<StringTokenizer, Complex128> parseFunction = (StringTokenizer tokenizer) ->
+                new Complex128(Double.parseDouble(tokenizer.nextToken()), Double.parseDouble(tokenizer.nextToken()));
+        SparseMatrixData<Complex128> matData = loadCooMatrix(parseFunction, false, false, null);
+
+        if(!matData.shape().isSquare())
+            throw new Flag4jParsingException("Symmetric/Hermitian matrices must be square but got shape " + matData.shape());
+
+        List<Complex128> data = matData.data();
+        List<Integer> rowIndices = matData.rowData();
+        List<Integer> colIndices = matData.colData();
+        int size = data.size();
+
+        // Copy lower triangular values to upper triangular portion for symmetric/Hermitian matrix.
+        for(int i=0; i<size; i++) {
+            int row = rowIndices.get(i);
+            int col = colIndices.get(i);
+
+            if(row != col) {
+                data.add(isHerm ? data.get(i).conj() : data.get(i));
+                rowIndices.add(colIndices.get(i));
+                colIndices.add(rowIndices.get(i));
+            }
+        }
+
+        mat = new CooCMatrix(matData.shape(), data, rowIndices, colIndices).sortIndices();
+    }
+
+
+    /**
+     * Loads a complex COO matrix from a Matrix Market Format file.
+     * @param isPattern Flag indicating if this matrix is a pattern matrix ({@code true}) or a numerical matrix ({@code false}).
+     * @throws IOException If an I/O error occurs.
+     */
+    private void loadCooComplexMatrix(boolean isPattern) throws IOException {
         Function<StringTokenizer, Complex128> parseFunction = (StringTokenizer tokenizer) ->
             new Complex128(Double.parseDouble(tokenizer.nextToken()), Double.parseDouble(tokenizer.nextToken()));
-        SparseMatrixData<Complex128> matData = loadCooMatrix(parseFunction);
+        SparseMatrixData<Complex128> matData = loadCooMatrix(parseFunction, true, isPattern, Complex128.ONE);
         mat = new CooCMatrix(matData.shape(), matData.data(), matData.rowData(), matData.colData());
     }
 
@@ -335,13 +508,20 @@ public class MatrixMarketReader {
     /**
      * Loads a complex COO matrix from a Matrix Market Format file.
      * @param parseFunction Function to parse {@link StringTokenizer} for a single data entry to the field type {@link T}.
+     * @param sort Flag indicating if lexicographical sorting should be done on the entries of the matrix. If {@code true}, sorting
+     * will be performed; otherwise if {@code false}, <em>no</em> sorting will be performed.
+     * @param isPattern Flag indicating if the matrix being loaded is a pattern matrix ({@code true}) or a numerical matrix
+     * ({@code false}).
+     * @param patternDefault Default value to use when {@code isPattern == true}. If {@code isPattern == false} this will be ignored
+     * and may be {@code null}.
      * @return A {@link SparseMatrixData} object storing the shape, non-zero data, row indices, and column indices of the COO
      * matrix.
      * @throws IOException If an I/O error occurs.
      * @param <T> Type corresponding to the field from the Matrix Market file header.
      * e.g. real &rarr; {@code T} is type {@link Double}, complex &rarr; {@code T} is type {@link Complex128}.
      */
-    private <T> SparseMatrixData<T> loadCooMatrix(Function<StringTokenizer, T> parseFunction) throws IOException {
+    private <T> SparseMatrixData<T> loadCooMatrix(
+            Function<StringTokenizer, T> parseFunction, boolean sort, boolean isPattern, T patternDefault) throws IOException {
         int[] shape = parseDimensions(3);
         int rows = shape[0];
         int cols = shape[1];
@@ -365,14 +545,14 @@ public class MatrixMarketReader {
             // Indices are one based in Matrix Market Format files so shift down by one.
             rowIndices.add(Integer.parseInt(tokenizer.nextToken()) - 1);
             colIndices.add(Integer.parseInt(tokenizer.nextToken()) - 1);
-            data.add(parseFunction.apply(tokenizer));
-            int endTokens = tokenizer.countTokens();
+            data.add(isPattern ? patternDefault : parseFunction.apply(tokenizer));
 
             if(tokenizer.hasMoreTokens()) {
+                int endTokens = tokenizer.countTokens();
                 int expectedTokens = startTokens - endTokens;
                 throw new Flag4jParsingException("Expecting exactly " + expectedTokens + " values in Matrix Market " +
-                        "file for " + header.field() + " coordinate Format " +
-                        "but found " + tokenizer.countTokens() + " on line " + currLineNumber + ".");
+                        "file for " + header.field() + " coordinate format " +
+                        "but found " + endTokens + " on line " + currLineNumber + ".");
             }
         }
 
@@ -382,8 +562,61 @@ public class MatrixMarketReader {
                     "The number of  data lines does not match the number of non-zero data specified: " + cols + ".");
         }
 
-        // Ensure that the data is sorted by indices lexicographically as this is not required by Matrix Market.
-        CooDataSorter<T> sorter = new CooDataSorter<>(data, rowIndices, colIndices).sparseSort();
+        if(sort) {
+            // Ensure that the data is sorted by indices lexicographically as this is not required by Matrix Market.
+            new CooDataSorter<>(data, rowIndices, colIndices).sparseSort();
+        }
+
         return new SparseMatrixData<T>(new Shape(rows, cols), data, rowIndices, colIndices);
+    }
+
+
+    /**
+     * Loads a dense symmetric/Hermitian matrix from a Matrix Market Format file.
+     * @param parseFunction Function to parse {@link StringTokenizer} for a single data entry to the field type {@link T}.
+     * @return A {@link Pair} containing the {@link Shape shape} of the matrix and a list containing the data of the matrix in
+     * colum-major ordering.
+     * @throws IOException If an I/O error occurs.
+     * @param <T> Type corresponding to the field from the Matrix Market file header.
+     * e.g. real &rarr; {@code T} is type {@link Double}, complex &rarr; {@code T} is type {@link Complex128}.
+     */
+    private <T> Pair<Shape, List<T>> loadDenseSymmMatrix(Function<String, T> parseFunction) throws IOException {
+        Shape shape = new Shape(parseDimensions(2));
+        int rows = shape.get(0);
+        int cols = shape.get(1);
+        int lineLength = 2*rows;
+
+        if (rows != cols) {
+            throw new Flag4jParsingException("Expecting symmetric matrix to be square but got shape "
+                    + shape + ".");
+        }
+
+        final int size = (rows * (rows + 1)) / 2;
+        List<T> data = new ArrayList<T>(size);
+
+        // MatrixMarket stores matrices in column major ordering.
+        int idx = 0;
+        for(int i=0; i<size; i++) {
+            findNextNonemptyLine();
+            if (currLine == null) {
+                throw new Flag4jParsingException("Expecting " + size +
+                        " data in the Matrix Market file but found " + (i+1) + ".");
+            }
+
+            try {
+                data.add(parseFunction.apply(currLine));
+            } catch(NullPointerException e) {
+                throw new Flag4jParsingException("Expecting " + size +
+                        " data in the Matrix Market Format file but found " + (i+1) + ".");
+            }
+        }
+
+        findNextNonemptyLine();
+        if(currLine != null) {
+            throw new Flag4jParsingException("Found extra non-empty lines at the end of the Matrix Market Format file. " +
+                    "The number of data lines does not match the number of columns specified: " + cols + ".");
+        }
+
+        return new Pair<>(shape, data);
     }
 }
